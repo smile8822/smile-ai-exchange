0d55197eb0ec6ac466ab160134763d674adde321
smile8822 <mgcms0502@gmail.com>
2025-12-04 21:03:17 +0900

    Update WEBHOOK_URL to test environment

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-const WEBHOOK_URL = "https://smile8822.app.n8n.cloud/webhook/smile-exchange";
+const WEBHOOK_URL = "https://smile8822.app.n8n.cloud/webhook/smile-exchange-test";
 export { WEBHOOK_URL };
-
